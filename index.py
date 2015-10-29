cebe8168700db942fcdb26fdb91986b2c937068d
TomChambrier <tomw08@gmail.com>
2015-10-29 22:34:03 +0000

    next

--- index.py
+++ index.py
@@ -1,11 +1,9 @@
-__doc__ = """
-Main file that creates rendering window and defines the game loop
-"""
 import pygame
+pygame.init()
+import NN_output
 import Class_Ball
 import Class_Bar
 
-pygame.init()
 clock = pygame.time.Clock()
 
 # neural network in action the longer you play the smarter it gets
@@ -22,7 +20,7 @@ bar_length = 100
 bar_width = 10
 
 
-gameDisplay = pygame.display.set_mode((window_width, 600))
+gameDisplay = pygame.display.set_mode((window_width, window_height))
 
 pygame.display.set_caption('Pong')
 
@@ -33,26 +31,29 @@ gameExit = False
 lead_y_change = 0
 
 bar_player = Class_Bar.Bar(80, 30, 50, 300)
+bar_ai = Class_Bar.Bar(80, 30, 730, 300)
+
 ball = Class_Ball.Ball(400, 300, white, 20)
 
+# hold coordinates for players bar
 lead_x = bar_player.x_pos_center
 lead_y = bar_player.y_pos_center
+
+# hold coordinates for ai bar
+ai_bar_x = bar_ai.x_pos_center
+ai_bar_y = bar_ai.y_pos_center
+
 # Game Loop
 while not gameExit:
 
     for event in pygame.event.get():
-        #  print(event)
+        #print('bottom y boundary ' + str(bar_player.bottom_y_boundary()))
         if event.type == pygame.QUIT:
             gameExit = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                lead_y_change = -4
-            if event.key == pygame.K_DOWN:
-                lead_y_change = 4
+
     lead_y = (pygame.mouse.get_pos()[1]) - 50
 
-    # speed at which ball moves from initial position
-    ball.update_pos()
+    ball.update_pos()   # calls function to get new position of ball
 
     lead_y += lead_y_change
     if lead_y >= 495 or lead_y < 0:
@@ -61,12 +62,14 @@ while not gameExit:
     gameDisplay.fill(black)
     pygame.draw.circle(gameDisplay, ball.colour, [ball.x_pos, ball.y_pos], ball.radius)
     pygame.draw.rect(gameDisplay, white, [lead_x, lead_y, bar_width, bar_length])
+    pygame.draw.rect(gameDisplay, white, [ai_bar_x, ai_bar_y, bar_width, bar_length])
+
    # pygame.draw.rect(gameDisplay, white, [bar_player.x_pos_center, bar_player.y_pos_center,
     #                                      bar_width, bar_length])
 
     # push  input data for NN to array for given frame(ball centre pos + player bar y_pos)
-    pygame.display.update()
-    clock.tick(200)
+    pygame.display.update()     # next frame
+    clock.tick(200)     # fps
 
 pygame.quit()
 quit()

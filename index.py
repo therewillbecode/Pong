3b8ba7f5b0dc2b80f475a50988720582d0cea5b6
carolinux <karolina.alexiou@teralytics.ch>
2015-10-30 19:28:33 +0100

    Made the index.py have a main function

--- index.py
+++ index.py
@@ -1,21 +1,10 @@
 __doc__ = """Main game loop and initialisation of game window"""
+
 import pygame
-pygame.init()
+
 import Class_Ball
 import Class_Bar
 from Instantiate_Initial_Objs import Draw_Init_Objs
-
-clock = pygame.time.Clock()
-
-# neural network in action the longer you play the smarter it gets
-# score is a function of time played and level of iteration of neural net
-# draw diagram of neural network layers as background
-
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
-window_width = 800
-window_height = 600
 
 bar_length = 100
 bar_width = 10
@@ -25,42 +14,59 @@ def Draw_Shapes():
     pygame.draw.rect(gameDisplay, white, [lead_x, lead_y, bar_width, bar_length])
     pygame.draw.rect(gameDisplay, white, [ai_bar_x, ai_bar_y, bar_width, bar_length])
 
-gameDisplay = pygame.display.set_mode((window_width, window_height))
+def main():
+    pygame.init()
 
-pygame.display.set_caption('Pong')
+    clock = pygame.time.Clock()
 
-pygame.display.update()
+    # neural network in action the longer you play the smarter it gets
+    # score is a function of time played and level of iteration of neural net
+    # draw diagram of neural network layers as background
 
-gameExit = False
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    red = (255, 0, 0)
+    window_width = 800
+    window_height = 600
+    gameDisplay = pygame.display.set_mode((window_width, window_height))
 
-lead_y_change = 0
+    pygame.display.set_caption('Pong')
 
-Draw_Init_Objs()
+    pygame.display.update()
+
+    gameExit = False
+
+    lead_y_change = 0
+
+    Draw_Init_Objs()
 
 # Game Loop
-while not gameExit:
+    while not gameExit:
 
-    for event in pygame.event.get():
-        #print('top y boundary ' + str(Class_Bar.Bar.top_y_boundary(bar_player)))
-        #print('bottom y boundary ' + str(Class_Bar.Bar.bottom_y_boundary(bar_player)))
-        #print((Class_Bar.Bar.top_y_boundary(bar_player)) - Class_Bar.Bar.bottom_y_boundary(bar_player)) # set up test that says that diff should equal length
-        if event.type == pygame.QUIT:
-            gameExit = True
+        for event in pygame.event.get():
+            #print('top y boundary ' + str(Class_Bar.Bar.top_y_boundary(bar_player)))
+            #print('bottom y boundary ' + str(Class_Bar.Bar.bottom_y_boundary(bar_player)))
+            #print((Class_Bar.Bar.top_y_boundary(bar_player)) - Class_Bar.Bar.bottom_y_boundary(bar_player)) # set up test that says that diff should equal length
+            if event.type == pygame.QUIT:
+                gameExit = True
 
-    lead_y = (pygame.mouse.get_pos()[1]) - 50
+        lead_y = (pygame.mouse.get_pos()[1]) - 50
 
-    ball.update_pos()   # calls function to get new position of ball
+        ball.update_pos()   # calls function to get new position of ball
 
     # ensures bar is constrained to position within window border
-    lead_y += lead_y_change
-    if lead_y >= 495 or lead_y < 0:
-        lead_y_change *= -1
+        lead_y += lead_y_change
+        if lead_y >= 495 or lead_y < 0:
+            lead_y_change *= -1
 
-    gameDisplay.fill(black)
+        gameDisplay.fill(black)
 
-    Draw_Shapes()
-    pygame.display.update()     # next frame
-    clock.tick(200)     # fps
+        Draw_Shapes()
+        pygame.display.update()     # next frame
+        clock.tick(200)     # fps
 
-pygame.quit()
-quit()
+    pygame.quit()
+    quit()
+
+if __name__ == '__main__':
+    main()

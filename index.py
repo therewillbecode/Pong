68b68862f3e6ec8266758dd44f84dad44cf29130
carolinux <karolina.alexiou@teralytics.ch>
2015-10-30 19:41:02 +0100

    Put logit to update bar position in separate function

--- index.py
+++ index.py
@@ -23,6 +23,8 @@ def main():
     # score is a function of time played and level of iteration of neural net
     # draw diagram of neural network layers as background
 
+    # Suggestion: You could have a method that initializes the GUI
+
     white = (255, 255, 255)
     black = (0, 0, 0)
     red = (255, 0, 0)
@@ -40,24 +42,30 @@ def main():
 
     Draw_Init_Objs()
 
-# Game Loop
+    # Game Loop
     while not gameExit:
 
         for event in pygame.event.get():
+            #pip install ipdb
+            #then whenever you want to stop exectuion and inspect the program state
+            #import ipdb; ipdb.set_trace() # this sets a breakpoint
+            #bar_player
+            # this outputs the value of the variable
+            # c to continue
+            # n to go to next line
+            # s to step inside function
             #print('top y boundary ' + str(Class_Bar.Bar.top_y_boundary(bar_player)))
             #print('bottom y boundary ' + str(Class_Bar.Bar.bottom_y_boundary(bar_player)))
             #print((Class_Bar.Bar.top_y_boundary(bar_player)) - Class_Bar.Bar.bottom_y_boundary(bar_player)) # set up test that says that diff should equal length
             if event.type == pygame.QUIT:
                 gameExit = True
 
-        lead_y = (pygame.mouse.get_pos()[1]) - 50
+        lead_y = (pygame.mouse.get_pos()[1]) - bar_length/2
 
         ball.update_pos()   # calls function to get new position of ball
 
-    # ensures bar is constrained to position within window border
-        lead_y += lead_y_change
-        if lead_y >= 495 or lead_y < 0:
-            lead_y_change *= -1
+        # ensures bar is constrained to position within window border
+        lead_y, lead_y_change = update_player_bar(lead_y, lead_y_change, window_height, bar_length)
 
         gameDisplay.fill(black)
 
@@ -67,6 +75,13 @@ def main():
 
     pygame.quit()
     quit()
+
+def update_player_bar(lead_y, lead_y_change, window_height, bar_length ):
+    lead_y += lead_y_change
+    if lead_y >= window_height - 2* bar_length or lead_y < 0:
+        lead_y_change *= -1
+
+    return lead_y, lead_y_change
 
 if __name__ == '__main__':
     main()

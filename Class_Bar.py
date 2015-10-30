3bc650bba778a3df50ffd387253f755622c81691
TomChambrier <tomw08@gmail.com>
2015-10-30 11:37:41 +0000

    changed indentation2

--- Class_Bar.py
+++ Class_Bar.py
@@ -2,8 +2,10 @@ __author__ = 'Tom'
 import pygame
 import index
 __doc__ ="""
-class for paddle bar used to hit ball
 """
+
+
+#   class for paddle bar used to hit ball
 class Bar:
 
     def __init__(self, length, width, start_x, start_y):
@@ -11,17 +13,20 @@ class Bar:
         self.y_pos_center = start_y
         self.length = length
         self.width = width
-        pygame.draw.rect(index.gameDisplay, index.white, [start_x, start_y,
-                                              width, length])
+        pygame.draw.rect(index.gameDisplay, index.white, [start_x, start_y, width, length])
 
+    @classmethod
     def right_edge_x_boundary(self):
-        return index.lead_x + (0.5 * self.width)
+        return self.x_pos_center + (0.5 * self.width)
 
+    @classmethod
     def left_x_boundary(self):
-        return index.lead_x - (0.5 * self.width)
+        return self.x_pos_center - (0.5 * self.width)
 
+    @classmethod
     def top_y_boundary(self):
-        return index.lead_y - (0.5 * self.length)
+        return self.y_pos_center - (0.5 * self.length)
 
+    @classmethod
     def bottom_y_boundary(self):
-        return index.lead_y + (0.5 * self.length)
+        return self.y_pos_center + (0.5 * self.length)

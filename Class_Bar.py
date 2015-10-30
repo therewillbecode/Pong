1ca251e3cb656c70c1bb17581776ea2bf32fdca6
carolinux <karolina.alexiou@teralytics.ch>
2015-10-30 20:11:48 +0100

    Refactoring class bar

--- Class_Bar.py
+++ Class_Bar.py
@@ -1,32 +1,42 @@
 __author__ = 'Tom'
 import pygame
-import index
 __doc__ ="""
 """
 
 
 #   class for paddle bar used to hit ball
 class Bar:
+    cnt = 0 # cnt would be shared by all instances of the class 
 
     def __init__(self, length, width, start_x, start_y):
         self.x_pos_center = start_x
         self.y_pos_center = start_y
         self.length = length
         self.width = width
-        pygame.draw.rect(index.gameDisplay, index.white, [start_x, start_y, width, length])
+        self.__class_.cnt+=1
+
+    def draw(self, gameDisplay, color):
+        pygame.draw.rect(gameDisplay, color, [self.x_pos_center, self.y_pos_center, self.width, self.length])
+        
 
     @classmethod
-    def right_edge_x_boundary(cls, bar):
-        return bar.x_pos_center + (0.5 * bar.width)
+    def getCount(cls):
+        return cls.cnt
+    #Bar.getCount()
 
-    @classmethod
-    def left_x_boundary(cls, bar):
-        return bar.x_pos_center - (0.5 * bar.width)
+    @staticmethod
+    def example():
+        return 1+2
+    # Math.sqrt()
 
-    @classmethod
-    def top_y_boundary(cls, bar):
-        return pygame.mouse.get_pos()[1] - (0.5 * bar.length)
+    def right_edge_x_boundary(self):
+        return self.x_pos_center + (0.5 * self.width)
 
-    @classmethod
-    def bottom_y_boundary(cls, bar):
-        return pygame.mouse.get_pos()[1] + (0.5 * bar.length)
+    def left_x_boundary(self):
+        return self.x_pos_center - (0.5 * self.width)
+
+    def top_y_boundary(self):
+        return pygame.mouse.get_pos()[1] - (0.5 * self.length)
+
+    def bottom_y_boundary(self):
+        return pygame.mouse.get_pos()[1] + (0.5 * self.length)
